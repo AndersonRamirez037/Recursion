308abd8a76dd36a938e5928fa27d77358929faa0
AndersonRamirez037 <andersongamer037@gmail.com>
2023-05-12 15:48:53 -0500

    6. Inception. Is it possible?

---
+++ 6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int is_prime(int n, int i){
+    if(n <= i){
+        return 0; 
+    }
+    if(n % i == 0 && i > 1){
+        return 0; 
+    }
+    if((n / i) < i){
+        return 1; 
+    }
+    return(is_prime(n,i + 1));
+}
+int is_prime_number(int n){
+    if(n < 2){
+        return 0;
+    }
+    return is_prime(n, 2);
+}
